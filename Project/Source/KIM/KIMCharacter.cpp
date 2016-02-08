9c6362a005be0212d35f9a89282d63c69554bdc2
JackHarb89 <steven.bartels@icloud.com>
2016-02-08 20:59:48 +0100

    +added simple main menu + added prottype fire

--- Project/Source/KIM/KIMCharacter.cpp
+++ Project/Source/KIM/KIMCharacter.cpp
@@ -151,9 +151,6 @@ void AKIMCharacter::ThrowObject() {
 // Called when the game starts or when spawned
 void AKIMCharacter::BeginPlay() {
 	Super::BeginPlay();
-// 	if (GetController()) {
-// 		((AKIMPlayerController*)GetController())->bShowMouseCursor = true;
-// 	}
 }
 
 // Called every frame
